6d03d5e7f078446cc44da80e24b0dbbf3caf3c5e
Justin Bitely <jbitely@gmail.com>
2016-02-03 18:51:08 -0500

    Built basic http method handling structure in request-handler.

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,31 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
+var actions = {
+  'GET': function(req, res) {
+    console.log("request received: " + req);
+    res.end();
+  },
+  'POST': function(req, res) {
+
+  },
+  'OPTIONS': function(req, res) {
+
+  }
+};
 exports.handleRequest = function (req, res) {
+  var action = req.method;
+
+  if (action) {
+    actions[action](req, res);
+  } else {
+    // TODO fix error!
+    console.log("Whoops! There's an error.");
+    res.end();
+  }
+
+
   res.end(archive.paths.list);
 };
